0ff7c3f513e27d16ea9da7a5d5ae001ba219f606
shawnchampion <shawnchampion2016@gmail.com>
2023-09-08 15:56:06 +0800

    2023/09/08 2651. 计算列车到站时间

---
+++ src/code/y2023/m09/D08P2651.java
@@ -0,0 +1,7 @@
+package code.y2023.m09;
+
+public class D08P2651 {
+    public int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+        return (arrivalTime + delayedTime) % 24;
+    }
+}
